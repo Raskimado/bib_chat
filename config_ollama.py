1d01568c243e987c82b087817b3ad6dde709eaa3
Raskimado <martin.brossard@hotmail.com>
2025-06-28 19:11:40 +0200

    Update config_ollama.py

--- config_ollama.py
+++ config_ollama.py
@@ -12,9 +12,22 @@ llm = OllamaLLM(
 #define prompt
 prompt = ChatPromptTemplate.from_messages(
     [
-    ("system", "Answer the user's questions based on the context: {context}."
-    "Be concise but use at least on sentence."
-    "Based on the context, the response should contain three titles."
+    ("system",
+     "You are a recommendation AI. You recommendations include books, e-books,"
+    "print journals und e-journals."
+    "Answer the user's questions based on the context {context} provided, but"
+    "do not mention the context or retrieval process. If you do not have enough"
+    "information to answer, simply say you are unsure or cannot answer."
+    "If you lack information, respond with 'I am not sure.'"
+    "Only give possible book titles if you are very sure. Otherwise, answer that"
+    "you don't know any suitable titles or only have a few guesses."
+    "Be berief, concise and friendly. Use at least on sentence."
+    "Aks clarifying questions if needed to better understand the query."
+    "If possible make sure the response contains three titles."
+    "If you do not find three matching title, be truthful and just mention what"
+    "you found."
+    "If you do not find anything, excuse yourself and tell the user you did not"
+    "find anything."
     "If you do not find titles based on the context, say it accordingly."
     "Never mention the MMS-ID."),
     # Used as a placeholder for list of messages
